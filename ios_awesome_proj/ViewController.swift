ffc1fb57463d0b782fa2c6769611c6dc411fde67
codermy <gsmxsky@gmail.com>
2024-04-23 11:59:15 +0800

    init

---
+++ ios_awesome_proj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_awesome_proj
+//
+//  Created by codermy on 2024/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
